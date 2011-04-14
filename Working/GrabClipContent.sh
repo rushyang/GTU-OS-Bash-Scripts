04869c02ca55cf1ad5d28920bc3062b2d18490fd
Rushyang Darji <rushyangdarji@gmail.com>
2011-04-14 19:25:41 +0530

    edited ‘Working/GrabClipContent.sh’

--- Working/GrabClipContent.sh
+++ Working/GrabClipContent.sh
@@ -1,18 +1,21 @@
+#!/usr/bin/env bash
 # This script constantly checks the clipboard content and if new entry is found, then will copy that into a specific file
-gather='/home/rushyang/Aarav23032011'
-if [[ ! -f $gather ]]; then
-	touch $gather
-fi
+while true; do
+	targetfile=`zenity --entry --text="Enter the file name: " --title="Initiating Capture Clipboard Script.." --width=400`
+	gather="$HOME/$targetfile"
+	[[ -f $gather ]] && dup=`zenity --question --text="This file already exists, Are you sure you want to continue?" --width=500; echo $?`
+	[[ "$dup" -ne "1" ]] && break
+done
 echo '' | xclip -selection clipboard
-old=''
 while true; do
 	content=`xclip -selection clipboard -o`
-	if [[ "$old" != "$content" ]]; then
+	if [[ "$content" != '' ]]; then
 		if [[ "$content" = 'exit' ]]; then
-			exit
+			notify-send "Capture Script Terminated!" -i gtk-dialog-info && exit
 		fi
 		echo "$content" >> "$gather"
-		old=`echo $content`
+		echo '' | xclip -selection clipboard
+		#zenity --notification --timeout=1 && sleep 1
 	fi
 	sleep 1
 done
